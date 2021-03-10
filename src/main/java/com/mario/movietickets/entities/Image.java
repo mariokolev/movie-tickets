42b76b1f7f9630d40b208707253bad62d6a42454
mariokolev <mario.kolevv@gmail.com>
2021-03-10 21:31:06 +0200

    added Image Entity

---
+++ src/main/java/com/mario/movietickets/entities/Image.java
@@ -0,0 +1,62 @@
+package com.mario.movietickets.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Lob
+    @Column(name = "image_bytes")
+    private byte[] imageBytes;
+
+    public Image(){}
+
+    public Image(String name, String type, byte[] imageBytes){
+        this.name = name;
+        this.type = type;
+        this.imageBytes = imageBytes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getImageBytes() {
+        return imageBytes;
+    }
+
+    public void setImageBytes(byte[] imageBytes) {
+        this.imageBytes = imageBytes;
+    }
+}
